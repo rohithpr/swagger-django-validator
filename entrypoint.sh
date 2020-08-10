75bfab60739a6117e9539d1baa09e93a7b5ace8e
Rohith PR <praroh2@gmail.com>
2020-08-11 02:35:16 +0530

    Run post_install script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ pip install -r requirements.txt
 # Install pyyaml to read the Swagger file.
 pip install pyyaml
 
+# TODO: Formalize hook scripts?
+# TODO: Check if post_install script exists before running it
+sh bin/.swagger_django_validator/post_install.sh
+
 # Copy required files into the workspace.
 # TODO: Copying it to the workspace from the Dockerfile itself fails. Why?
 # Perhaps something to do with the `checkout` action?
