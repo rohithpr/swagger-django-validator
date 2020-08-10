9a0a3ebc5abe40abe185fe10f74b207fb1328462
Rohith PR <praroh2@gmail.com>
2020-08-11 02:56:14 +0530

    Allow filtering for unresolable endpoints

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ import os
 import django
 import yaml
 from django.urls import URLPattern, URLResolver, get_resolver
+from django.urls.exceptions import Resolver404
 
 django.setup()
 
@@ -21,7 +22,20 @@ def read_swagger_file(file_path):
 
 
 def get_swagger_endpoints(file_contents):
-    return file_contents["paths"]
+    return file_contents["paths"].keys()
+
+
+def get_unresolvable_endpoints(resolver, endpoints):
+    unresolvable_endpoints = []
+    for endpoint in endpoints:
+        try:
+            # Note: This isn't really a perfect way of doing what we wish to do here.
+            # /domain/resource in a swagger file could end up matching /domain/{id}, for instance.
+            # However, it's good enough for current use cases.
+            _ = resolver.resolve(endpoint)
+        except Resolver404:
+            unresolvable_endpoints.append(endpoint)
+    return unresolvable_endpoints
 
 
 def main():
@@ -29,7 +43,9 @@ def main():
     swagger_file_path = get_env_var("SWAGGER_FILE_PATH")
     swagger_contents = read_swagger_file(swagger_file_path)
     swagger_endpoints = get_swagger_endpoints(swagger_contents)
-    print(swagger_endpoints)
+    unresolvable_swagger_endpoints = get_unresolvable_endpoints(
+        resolver, swagger_endpoints
+    )
 
 
 if __name__ == "__main__":
