9597fe89c21b347da76dc6d70b709c55e4e9d3ff
Rohith PR <praroh2@gmail.com>
2020-08-11 03:29:10 +0530

    Read django endpoints and filter for resolvable endpoints

--- controller.py
+++ controller.py
@@ -25,6 +25,45 @@ def get_swagger_endpoints(file_contents):
     return file_contents["paths"].keys()
 
 
+def get_clean_django_endpoint(parts):
+    """Turn a list of path parts into a clean path.
+
+    Example: ["^", "^login/$"] -> /login/
+    """
+    path = ""
+    for part in parts:
+        part = part.lstrip("^").rstrip("$")
+        # TODO: Can this be replaced with anything but an empty string?
+        part = part.replace("\\.(?P<format>[a-z0-9]+)/?", "")
+        part = part.replace("(?P<pk>[^/.]+)", "1")
+        if part:
+            path = "/".join((path, part))
+    path = path.replace("//", "/")
+    return path
+
+
+def get_django_endpoints(url_patterns):
+    def list_urls(lis, acc=None):
+        # https://stackoverflow.com/a/54531546
+        # TODO: Clean up and move this out.
+        if acc is None:
+            acc = []
+        if not lis:
+            return
+        l = lis[0]
+        if isinstance(l, URLPattern):
+            yield acc + [str(l.pattern)]
+        elif isinstance(l, URLResolver):
+            yield from list_urls(l.url_patterns, acc + [str(l.pattern)])
+        yield from list_urls(lis[1:], acc)
+
+    endpoints = []
+    for path_parts in list_urls(url_patterns):
+        endpoint = get_clean_django_endpoint(path_parts)
+        endpoints.append(endpoint)
+    return endpoints
+
+
 def get_unresolvable_endpoints(resolver, endpoints):
     unresolvable_endpoints = []
     for endpoint in endpoints:
@@ -49,9 +88,23 @@ def get_resolvable_swagger_endpoints(resolver, *, log_unresolvable_paths=True):
     return resolvable_endpoints, bool(unresolvable_endpoints)
 
 
+def get_resolvable_django_endpoints(resolver, *, log_unresolvable_paths=True):
+    endpoints = get_django_endpoints(resolver.url_patterns)
+    # TODO: Refactor
+    unresolvable_endpoints = get_unresolvable_endpoints(endpoints)
+    resolvable_endpoints = set(endpoints) - set(unresolvable_endpoints)
+    if log_unresolvable_paths:
+        print(f"Found the following unresolvable endpoints in the Django app: {unresolvable_endpoints}")
+        print(
+            "This shouldn't happen, and is likely to be an issue with the validation tool. Please report the issue to the maintainer of the project."
+        )
+    return resolvable_endpoints, bool(unresolvable_endpoints)
+
+
 def main():
     resolver = get_resolver()
     resolvable_swagger_endpoints, swagger_has_unresolvable_endpoints = get_resolvable_swagger_endpoints(resolver)
+    resolvable_django_endpoints, django_has_unresolvable_endpoints = get_resolvable_django_endpoints(resolver)
 
 
 if __name__ == "__main__":
